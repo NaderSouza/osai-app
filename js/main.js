1374598fd8bcc917cc78621efad0b3c1aa33c774
Henrique De Melo Cristioglu <henrique.cristioglu@threeo.com.br>
2023-08-24 08:33:44 -0300

    feat: login p

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 const inputs = document.querySelectorAll(".input");
 
 
-function addcl(){
+function addcl() {
 	let parent = this.parentNode.parentNode;
 	parent.classList.add("focus");
 }
 
-function remcl(){
+function remcl() {
 	let parent = this.parentNode.parentNode;
-	if(this.value == ""){
+	if (this.value == "") {
 		parent.classList.remove("focus");
 	}
 }
@@ -18,3 +18,21 @@ inputs.forEach(input => {
 	input.addEventListener("focus", addcl);
 	input.addEventListener("blur", remcl);
 });
+
+$('#formP').addEventListener('submit', onSubmit)
+
+function onSubmit(e) {
+	e.preventDefault()
+
+	const user = {
+		username: $('#username').value,
+		password: $('#password').value
+	}
+
+	localStorage.setItem('user', JSON.stringify(user))
+	window.location.replace('index.html')
+}
+
+function $(selector) {
+	return document.querySelector(selector)
+}
