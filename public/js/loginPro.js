ad49c4f529dc122750650226faed998286758e29
Nader Hadad Souza <naderhadadsouza@gmail.com>
2023-09-05 02:25:43 -0300

    App com Site

---
+++ public/js/loginPro.js
@@ -0,0 +1,35 @@
+$('#formLogin').addEventListener('submit', onSubmit)
+
+function onSubmit(e) {
+  e.preventDefault();
+
+
+  const xhr = new XMLHttpRequest();
+	xhr.open("GET", '/login-pro?username=' + $('#username').value + '&password=' + $('#password').value);
+	xhr.send();
+	xhr.responseType = "json";
+	xhr.onload = () => {
+
+		if (xhr.readyState != 4 || xhr.status != 200)
+		{
+			window.location.replace('LoginProErro.html');
+			return;
+		}
+
+		var response = xhr.response;
+		
+		if (response.status != 'ok')
+		{
+			window.location.replace('LoginProErro.html');
+			return;
+		}	
+
+		localStorage.setItem('user', response.user);
+		window.location.replace('MapaPro.html');
+		
+	};
+}
+
+function $(selector) {
+  return document.querySelector(selector)
+}
